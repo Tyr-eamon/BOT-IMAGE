909e17265b9c32102b05310df32e67aa8f33911c
Tyr-eamon <2409355340@qq.com>
2025-12-07 21:30:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -132,6 +132,7 @@ function getHeadStyle(title) {
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <title>${title}</title>
+<link rel="icon" href="https://link.tyrlink.dpdns.org/mtc.png" type="image/png">
 <style>
 :root { --bg-color: #020617; --header-bg: #0f172a; --card-bg: #1e293b; --text-primary: #f8fafc; --text-secondary: #94a3b8; --accent: #e11d48; --border: #334155; }
 * { box-sizing: border-box; }
@@ -527,4 +528,3 @@ async function renderAlbum(env, code, url) {
     
   return new Response(html, { headers: { "content-type": "text/html; charset=utf-8" } });
 }
-
